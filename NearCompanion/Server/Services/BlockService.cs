b057ee5044440fbfebcc62b89c83020b17e56671
Defikush <eugenporfire@gmail.com>
2022-10-31 20:27:11 +0100

    Reverted test

--- NearCompanion/Server/Services/BlockService.cs
+++ NearCompanion/Server/Services/BlockService.cs
@@ -79,11 +79,6 @@ namespace NearCompanion.Server.Services
 
                 while (keepPolling)
                 {
-                    if (blocks.Count >= 10)
-                    {
-                        keepPolling = false;
-                    }
-
                     BlockModel block = new BlockModel();
                     uint latency = 0;
 
@@ -135,7 +130,7 @@ namespace NearCompanion.Server.Services
             }
             finally
             {
-                //_ = Initialize();
+                _ = Initialize();
             }
         }
 
@@ -156,7 +151,7 @@ namespace NearCompanion.Server.Services
 
         private void HeightWatchdogElapsed(object? sender, ElapsedEventArgs e)
         {
-            //_ = ReadjustHeight();
+            _ = ReadjustHeight();
         }
 
         private async Task ReadjustHeight()
