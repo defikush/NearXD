2c959903f33e8665ab01e1072f493aa178f340bc
Defikush <eugenporfire@gmail.com>
2022-12-04 22:04:44 +0100

    Fixed the transactions list

--- NearCompanion/Client/Components/Block.razor.cs
+++ NearCompanion/Client/Components/Block.razor.cs
@@ -64,9 +64,9 @@ namespace NearCompanion.Client.Components
                     new Dictionary<string, object>() {{ "Model", chunk }}, 
                     new DialogOptions()
                     {
-                        Style = "border-radius: 25px; box-shadow: 3px 3px 0px 0px #5FB8FF, 0 12px 16px 0 #CAE6FF, 0 17px 50px 0 #CAE6FF; margin: 10px 50px 10px 10px;",
+                        Style = "border-radius: 25px; box-shadow: 3px 3px 0px 0px #5FB8FF, 0 12px 16px 0 #CAE6FF, 0 17px 50px 0 #CAE6FF;",
                         Width = "1200px",
-                        Height = "700px"
+                        Height = "700px",
                     });
             }
             catch (Exception ex)
